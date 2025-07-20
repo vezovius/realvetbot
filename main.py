17dab7b5788a628216407c89522aaa12df092d13
vezovius <aidinmahdavi@gmail.com>
2025-07-20 21:58:48 +0300

    Update main.py

--- main.py
+++ main.py
@@ -7,11 +7,11 @@ def start(update, context):
     update.message.reply_text("سلام! من فعال هستم.")
 
 def main():
-updater = Updater(TOKEN, use_context=True)
-dp = updater.dispatcher
-dp.add_handler(CommandHandler("start", start))
-updater.start_polling()
-updater.idle()
+    updater = Updater(TOKEN, use_context=True)
+    dp = updater.dispatcher
+    dp.add_handler(CommandHandler("start", start))
+    updater.start_polling()
+    updater.idle()
 
 if __name__ == '__main__':
 main()
