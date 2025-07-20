8aebdbb8a77ca6c35a839ed9acf8c1b22cbc5af0
vezovius <aidinmahdavi@gmail.com>
2025-07-20 17:58:01 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,10 @@
 from telegram.ext import ApplicationBuilder, CommandHandler
 import os
 
-TOKEN = os.getenv("BOT_TOKEN")
-app = ApplicationBuilder().token(TOKEN).build()
-
 async def start(update, context):
-    await update.message.reply_text("سلام! من فعال هستم.")
+await update.message.reply_text("سلام! من فعال هستم.")
 
+if __name__ == "__main__":
+app = ApplicationBuilder().token(os.getenv("BOT_TOKEN")).build()
 app.add_handler(CommandHandler("start", start))
-
-if __name__ == '__main__':
-    app.run_polling()
+app.run_polling()
