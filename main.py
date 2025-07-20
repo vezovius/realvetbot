d40f43a6706a9b0ae77c30fb18b886c49316def6
vezovius <aidinmahdavi@gmail.com>
2025-07-20 17:49:09 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,13 @@
+from telegram.ext import ApplicationBuilder, CommandHandler
+import os
+
+TOKEN = os.getenv("BOT_TOKEN")
+app = ApplicationBuilder().token(TOKEN).build()
+
+async def start(update, context):
+    await update.message.reply_text("سلام! من فعال هستم.")
+
+app.add_handler(CommandHandler("start", start))
+
+if __name__ == '__main__':
+    app.run_polling()
